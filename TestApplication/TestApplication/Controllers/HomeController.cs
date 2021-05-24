fd5fadbfdf50953f93299e9ae4d3b82537facd69
Lakhwinderchdit <lakhwinder.chdit@gmail.com>
2021-05-24 22:13:46 +0530

    commit

---
+++ TestApplication/TestApplication/Controllers/HomeController.cs
@@ -0,0 +1,51 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using static TestApplication.Models.TemplateModel;
+
+namespace TestApplication.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+
+           
+            return View();
+        }
+
+        [HttpGet]
+        public JsonResult GetHtml()
+        {
+            string FilePath = Server.MapPath("~/Scripts/Template/template.json");
+            dynamic JasonSchemaFile = string.Empty;
+            using (StreamReader r = new StreamReader(FilePath))
+            {
+                JasonSchemaFile = r.ReadToEnd();
+            }
+            return Json(JasonSchemaFile, JsonRequestBehavior.AllowGet);
+        }
+
+        public ActionResult Template()
+        {
+            return View();
+        }
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ TestApplication/TestApplication/Models/TemplateModel.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TestApplication.Models
+{
+    public class TemplateModel
+    {
+        public class Rootobject
+        {
+            public Schema schema { get; set; }
+        }
+        public class Schema
+        {
+            public Friends friends { get; set; }
+        }
+
+        public class Friends
+        {
+            public string type { get; set; }
+            public Items items { get; set; }
+        }
+
+        public class Items
+        {
+            public string type { get; set; }
+            public string title { get; set; }
+            public Properties properties { get; set; }
+        }
+
+        public class Properties
+        {
+            public Nick nick { get; set; }
+            public Gender gender { get; set; }
+            public Age age { get; set; }
+        }
+
+        public class Nick
+        {
+            public string type { get; set; }
+            public string title { get; set; }
+            public bool required { get; set; }
+        }
+
+        public class Gender
+        {
+            public string type { get; set; }
+            public string title { get; set; }
+            public string[] _enum { get; set; }
+        }
+
+        public class Age
+        {
+            public string type { get; set; }
+            public string title { get; set; }
+        }
+
+    }
+}
